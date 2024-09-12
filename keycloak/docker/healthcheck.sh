c73bc0eca094ada432c2b1448c21d0bbb98af80d
Ing. Petr Suchy <suchy@wamos.cz>
2024-09-12 17:51:36 +0200

    *edit - ES & Kibana 8.15.1 *fix - Keycloak Healthcheck - health is moved to management interface to port 9000 from Keycloak v25

--- keycloak/docker/healthcheck.sh
+++ keycloak/docker/healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-exec 3<>/dev/tcp/localhost/8080
+exec 3<>/dev/tcp/localhost/9000
 
-echo -e "GET /health/ready HTTP/1.1\nhost: localhost:8080\n" >&3
+echo -e "GET /health/ready HTTP/1.1\nhost: localhost:9000\n" >&3
 
 timeout --preserve-status 1 cat <&3 | grep -m 1 status | grep -m 1 UP
 ERROR=$?
